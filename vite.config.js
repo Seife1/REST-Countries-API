bea6413928955090d326fe8f93cacdd90531b35b
Seife1 <seifegebreal.mosisa@aait.edu.et>
2024-10-06 13:24:34 +0300

    base removed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/REST-Countries-API/',  // Set this to your GitHub repository name
   plugins: [react()],
 })
